886853aedc123f20b4cc868370acd6779012e773
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-12-28 09:57:27 +0100

    Only include on edit pages

--- source/php/App.php
+++ source/php/App.php
@@ -12,12 +12,31 @@ class App
         add_action('admin_enqueue_scripts', array($this, 'enqueueScripts'));
     }
 
+    public function isEditPage()
+    {
+        global $pagenow;
+
+        if (!is_admin()) {
+            return false;
+        }
+
+        if (get_post_type() === 'attachment') {
+            return false;
+        }
+
+        return in_array($pagenow, array('post.php', 'post-new.php'));
+    }
+
     /**
      * Enqueue required style
      * @return void
      */
     public function enqueueStyles()
     {
+        if (!$this->isEditPage()) {
+            return false;
+        }
+
         wp_register_style('content-scheduler', CONTENTSCHEDULER_URL . '/dist/css/content-scheduler.min.css', '', '1.0.0');
         wp_enqueue_style('content-scheduler');
     }
@@ -28,7 +47,11 @@ class App
      */
     public function enqueueScripts()
     {
-        wp_register_script('content-scheduler', CONTENTSCHEDULER_URL . '/dist/js/content-scheduler.dev.js', '', '1.0.0', true);
+        if (!$this->isEditPage()) {
+            return false;
+        }
+
+        wp_register_script('content-scheduler', CONTENTSCHEDULER_URL . '/dist/js/content-scheduler.min.js', '', '1.0.0', true);
         wp_enqueue_script('content-scheduler');
     }
 }
