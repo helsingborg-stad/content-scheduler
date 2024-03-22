51febdfc6818d6872bf9cc53df6623a6343894e1
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2024-03-22 15:21:14 +0100

    fix: activate npm section of build.php

--- build.php
+++ build.php
@@ -23,7 +23,7 @@ if(file_exists('composer.json')) {
     }
     $buildCommands[] = 'composer dump-autoload';
 }
-/*
+
 //Run npm if package.json is found
 if(file_exists('package.json') && file_exists('package-lock.json')) {
     $buildCommands[] = 'npm ci --no-progress --no-audit';
@@ -38,7 +38,6 @@ if(file_exists('package-lock.json') && !file_exists('gulp.js')) {
 } elseif(file_exists('package-lock.json') && file_exists('gulp.js') && is_array($argv) && in_array('--allow-gulp', $argv)) {
     $buildCommands[] = 'gulp';
 }
-*/
 
 // Files and directories not suitable for prod to be removed.
 $removables = [
