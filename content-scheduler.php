0032b74886514474cc2b73b65b434b74969f4a2f
GitHub Action <>
2024-03-22 14:21:49 +0000

    build: update package file versions to 3.1.1

--- content-scheduler.php
+++ content-scheduler.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Content Scheduler
  * Plugin URI:        https://github.com/helsingborg-stad/content-scheduler
  * Description:       Interface for post publishing and unpublisching schedulation
- * Version: 3.1.0
+ * Version: 3.1.1
  * Author:            Kristoffer Svanmark, Sebastian Thulin
  * Author URI:        https://github.com/helsingborg-stad
  * License:           MIT
